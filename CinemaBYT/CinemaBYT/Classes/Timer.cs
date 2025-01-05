618b659f2cf83ad0095f5d2bfe077ff8a9b0fdb1
Mykhailo Oliinyk <s27024@pjwstk.edu.pl>
2025-01-05 13:04:12 +0100

    update

---
+++ CinemaBYT/CinemaBYT/Classes/Timer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaBYT.Classes
+{
+    public class Timer
+    {
+        private DateTime dateTimeStart;
+        private DateTime dateTimeEnd;
+        Boolean isAvailable = true;
+        private Seat seat;
+        private Hall hall;
+        private DateTime DateTimeStart
+        {
+            get { return dateTimeStart; }
+            set { dateTimeStart = value; }
+        }
+
+        private DateTime DateTimeEnd
+        {
+            get { return dateTimeEnd; }
+            set { dateTimeEnd = value; }
+        }
+
+        private bool IsAvailable
+        {
+            get { return isAvailable; }
+            set { isAvailable = value; }
+        }
+
+        private Seat Seat
+        {
+            get { return seat; }
+            set { seat = value; }
+        }
+
+        private Hall Hall
+        {
+            get { return hall; }
+            set { hall = value; }
+        }
+    }
+
+}
